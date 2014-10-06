dda54f61c5f55fc77056aadb11929c383e3cb171
ryno <ryno412@gmail.com>
2014-10-06 09:44:48 -0700

    added jsonp

--- public/js/main.js
+++ public/js/main.js
@@ -11,20 +11,35 @@ var SimpleApp = {};
         },
         initEvents : function () {
             var self = this;
-           $('#do-something').click(function (e) {
-              self.displayView();
-           });
-        },
-        displayView : function () {
-            var $content = $('#content');
-            //in page template
-            //var source = $('#view-one').html();
-            //var template = Handlebars.compile(source);
-             //complied template
-            var template = Handlebars.templates['home'];
-            var html = template({name:'ryan'});
-            $content.html(html);
 
+            $('#do-something').click(function (e) {
+                $("#tag").val();
+                var tag = $("#tag").val() || "dog";
+                $.ajax({
+                    url: 'https://api.instagram.com/v1/tags/'+ tag +'/media/recent?client_id=4338f91f3dc247a48840875426772da6&callback',
+                    /*   jsonp: "JSON_CALLBACK",*/
+
+                    // tell jQuery we're expecting JSONP
+                    dataType: "jsonp",
+                    success: function( response ) {
+                        console.log( response ); // server response
+                        if (response && response.data) {
+                            self.displayPhotos(response.data);
+                        }
+                    },
+                    error : function () {
+
+                    }
+                });
+            });
+        },
+        displayPhotos : function (images) {
+            var source   = $("#photos").html();
+            var template = Handlebars.compile(source);
+            //var template = Handlebars.templates['home'];
+            var html    = template({images : images});
+            var $photoContainer = $("#content");
+            $photoContainer.html(html);
         }
 
 
